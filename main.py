9ad405c23bce4719fb6bd6d61ba07ea07d8f1aac
Arthur Neuman <arthur.neuman@gmail.com>
2016-01-09 22:00:04 -0500

    cleanup and sorting of results

--- main.py
+++ main.py
@@ -1,29 +1,34 @@
 import csv
 import os
 import sys
-from typing import Sequence, Tuple
+
+from collections import namedtuple
+from typing import List, Sequence, Tuple
+
+
+MatchResult = namedtuple("MatchResult", ["pb_numbers", "white", "red"])
 
 
 class PowerBallNumbers:
     def __init__(self, numbers: Sequence[int]):
-        self._red_ball = numbers[-1]
-        self._white_balls = set(numbers[:-1])
+        self.red_ball = numbers[-1]
+        self.white_balls = set(numbers[:-1])
 
     @property
     def numbers(self):
-        white = ", ".join([str(x) for x in sorted(self._white_balls)])
-        red = str(self._red_ball)
-        return "{0}, {1}".format(white, red)
+        all_balls = sorted(self.white_balls)
+        all_balls.append(self.red_ball)
+        return all_balls
 
     def __str__(self):
-        return "PowerBallNumber({0})".format(self.numbers)
+        return "PowerBallNumber({0})".format([str(x) for x in self.numbers])
 
     def check_match(self, drawing: Sequence[int]) -> Tuple[int, int]:
         drawing_red = drawing[-1]
         drawing_white = set(drawing[:-1])
 
-        red_match = 1 if self._red_ball == drawing_red else 0
-        white_match = len(self._white_balls & drawing_white)
+        red_match = 1 if self.red_ball == drawing_red else 0
+        white_match = len(self.white_balls & drawing_white)
         return white_match, red_match
 
 
@@ -36,12 +41,14 @@ class NumberLoader:
                 row = [int(x) for x in row]
                 self.numbers.append(PowerBallNumbers(row))
 
-    def get_results(self, drawing: Sequence[int]) -> Tuple:
+    def get_results(self, drawing: Sequence[int]) -> List[MatchResult]:
         results = []
+
         for number in self.numbers:
             white_result, red_result = number.check_match(drawing)
             if white_result >= 3 or red_result > 0:
-                results.append((number, (white_result, red_result)))
+                results.append(MatchResult(number, white_result, red_result))
+
         return results
 
 
@@ -51,13 +58,17 @@ def main(filename, drawing):
         return
 
     numbers = NumberLoader(filename)
-    matches = numbers.get_results(drawing)
-    for pb_numbers, (white_count, red_count) in matches:
-        message = ("W: {0}, R: {1} | {2}"
-                   .format(white_count, red_count, pb_numbers.numbers))
-        print(message)
+    matches = sorted(numbers.get_results(drawing),
+                     key=lambda x: x.white+x.red,
+                     reverse=True)
+
+    for match in matches:
+        numbers = ", ".join(str(x) for x in match.pb_numbers.numbers)
+        print("W: {0}, R: {1} | {2}"
+              .format(str(match.white), str(match.red), numbers))
 
 
 if __name__ == "__main__":
     if len(sys.argv) == 8:
-        main(sys.argv[1], [int(x) for x in sys.argv[2:]])
+        _, data_file, *draw_numbers = sys.argv
+        main(data_file, [int(x) for x in draw_numbers])
